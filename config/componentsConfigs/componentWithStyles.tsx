208b8b1fe8febd49211a06d9d2365dc618b63814
Shahar Eliyahu <shahar.e3@gmail.com>
2021-04-03 13:24:10 +0300

    Initial commit

---
+++ config/componentsConfigs/componentWithStyles.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles';
+
+
+const Component = ():React.FC => {
+  return (
+    <View>
+      <Text />
+    </View>
+  );
+};
+
+
+export default Component;
---
+++ index.js
@@ -0,0 +1,24 @@
+const inquirer = require("inquirer");
+const createComponent = require("./createComponent");
+
+const cli = async (allArgs) => {
+  const argv = allArgs.slice(2);
+  if (argv[0] === "cc") {
+    if (argv[1]) {
+      const [_, componentName, ...rest] = argv;
+      createComponent(componentName, rest);
+      return;
+    } else {
+      const { name } = await inquirer.prompt([
+        {
+          name: "name",
+          message: "What name should we call the component?",
+        },
+      ]);
+      const [_, ...rest] = argv;
+      createComponent(name, rest);
+    }
+  }
+};
+
+module.exports = { cli };
